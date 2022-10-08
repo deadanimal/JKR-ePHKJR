08d3254c9d6b5b07d8ea786e846dff405fa0e5a1
Afeez Aziz <afeezaziz@gmail.com>
2022-10-08 15:34:43 +0800

    123
    Co-authored-by: hayeh <hayeh@users.noreply.github.com>
    Co-authored-by: Maisarah Musa <maisarahmusa98@users.noreply.github.com>
    Co-authored-by: Zachroy-Anazfitry <Zachroy-Anazfitry@users.noreply.github.com>

--- resources/views/projek/senarai.blade.php
+++ resources/views/projek/senarai.blade.php
@@ -1,4 +1,22 @@
 @extends('layouts.app')
 @section('content')
     <h1>Senarai Projek</h1>
+
+    <table class="table table-bordered line-table" style="width:100%">
+        <thead class="text-white bg-orange-jkr">
+            <tr>
+                <th class="text-center">Bil</th>
+                <th class="text-center">ID Rujukan</th>
+                <th class="text-center">Nama Projek</th>
+                <th class="text-center">Alamat</th>
+                                            <th class="text-center">Status</th>
+                                                <th class="text-center">Tindakan</th> 
+                                            </tr>
+                                        </thead>
+
+
+
+
+
+
 @endsection
